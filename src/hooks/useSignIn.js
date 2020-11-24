3d0be36c950a7149cd1985b5efb26bef51d92d1d
NooraVino <noora.virolainen@yahoo.com>
2020-11-24 12:18:46 +0200

    exercise 10.15

--- src/hooks/useSignIn.js
+++ src/hooks/useSignIn.js
@@ -1,12 +1,19 @@
 import { SIGN_IN } from '../graphql/mutations'; 
-import {useMutation } from '@apollo/react-hooks';
-import AsyncStorage from '@react-native-community/async-storage';
+import {useMutation, useApolloClient  } from '@apollo/react-hooks';
+import { useContext } from 'react';
+import AuthStorageContext from '../contexts/AuthStorageContext';
 
 const useSignIn = () => {
+  const authStorage = useContext(AuthStorageContext);
   const [mutate, result] = useMutation(SIGN_IN);
+  const apolloClient = useApolloClient();
+
 
   const signIn = async ({ username, password }) => {
-    return await mutate({variables: { credentials: { username, password } },});
+    const {data} =  await mutate({variables: { credentials: { username, password } },});
+    await authStorage.setAccessToken(data.authorize.accesToken);
+    await apolloClient.resetStore();
+    return data;
   };
 
   return [signIn, result];
